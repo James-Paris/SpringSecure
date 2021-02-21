666ed83f7d98fa978b438b5812836b413bc7367a
James <James.Paris@Hcl.com>
2021-02-21 17:15:12 -0600

    Initial commit

---
+++ src/test/java/com/hcl/SpringSecure/SpringSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.SpringSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
